2717fe71bfd341c8008dbf711daac5bf61d2da9e
HarryBlackCatQAQ <641779160@qq.com>
2019-09-24 13:10:38 +0800

    1. MyCorsConfig 运行跨域 2. RedisConfig redis配置 3.更新了SpringSecurityConfig 配置  使用了 SpringSecurity + token + redis + vue 控制权限 4. TestController测试控制器 5. ValidateCodeController 验证码控制器 6.UserDao 添加了selectByUserName 查询方法 7. SpringSecurity JWTAuthenticationFilter 登录认证过滤器 8. SpringSecurity  JWTAuthorizationFilter 授权过滤器 9. SpringSecurity JWTLogoutFilter 登出过滤器 10. ImageCode 验证码实体类 11. ValidateCodeService 验证码服务 12. JWTRedisService token redis服务 13. JwtDate 自定义Date用于token 14. JwtUser 提供给SpringSecurity用于获取用户数据的载体 15. 自定义SpringSecurity UserDetailsService 获取用户数据、权限 16. JwtTokenUtils token帮助类 17. RedisUtil redis帮助类 18. SpringUtil手动获取bean 帮助类

--- src/main/java/com/bnuz/propertyManagementSystem/config/SpringSecurityConfig.java
+++ src/main/java/com/bnuz/propertyManagementSystem/config/SpringSecurityConfig.java
@@ -1,19 +1,111 @@
 package com.bnuz.propertyManagementSystem.config;
 
+import com.bnuz.propertyManagementSystem.filter.springsecurity.JWTAuthenticationFilter;
+import com.bnuz.propertyManagementSystem.filter.springsecurity.JWTAuthorizationFilter;
+import com.bnuz.propertyManagementSystem.filter.springsecurity.JWTLogoutFilter;
+import com.bnuz.propertyManagementSystem.filter.springsecurity.TokenClearLogoutHandler;
+import com.bnuz.propertyManagementSystem.springsecurity.impl.MyUserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
+ * SpringSecurityConfig配置
+ *
  * @Author: Harry
- * @Date: 2019-09-20 18:24
+ * @Date: 2019-09-23 00:42
  * @Version 1.0
  */
-@Configuration
-public class SpringSecurityConfig {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 自定义UserDetailsService 获取数据库中用户基本信息，和权限
+     * @return
+     */
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new MyUserDetailsServiceImpl();
+    }
+
+    /**
+     * 密码加密 采用SHA-256 + 随机盐 + 密钥对密码进行加密
+     * 密码进行hash处理，得到密码的hash值，然后将其与从数据库中查询到的密码hash值进行比较
+     * @return
+     */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 告诉AuthenticationManager要调用的userDetailsService和bCryptPasswordEncoder密码加密
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    /**
+     * SpringSecurity主要配置 配置api接口的权限和自定义的认证、授权等过滤器的添加
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+
+                // 测试用资源，需要验证了的用户才能访问
+                .antMatchers("/tasks/**").authenticated()
+                // 需要角色为xxx才能删除访问的资源
+                .antMatchers("/test/t1").hasAnyRole("0","1","2")
+                .antMatchers("/test/t2").hasAnyRole("1","2")
+                .antMatchers("/test/t3").hasAnyRole("2")
+                // 其他都放行了
+                .anyRequest().permitAll()
+                .and()
+
+                //添加过滤器
+                //登录认证过滤器
+                .addFilter(new JWTAuthenticationFilter(authenticationManagerBean()))
+                //用户api授权过滤器
+                .addFilter(new JWTAuthorizationFilter(authenticationManagerBean()))
+                //登出过滤器
+                .addFilter(new JWTLogoutFilter("/logout",tokenClearLogoutHandler()))
+                // 不需要session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                ;
+
+    }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public TokenClearLogoutHandler tokenClearLogoutHandler(){
+        return new TokenClearLogoutHandler();
+    }
+
+    /**
+     * 跨域设置
+     * @return
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }

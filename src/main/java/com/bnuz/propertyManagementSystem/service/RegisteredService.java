427514863da50f9138655cdd6ef6604873f46ea0
HarryBlackCatQAQ <641779160@qq.com>
2019-10-07 14:51:06 +0800

    1.添加注释

--- src/main/java/com/bnuz/propertyManagementSystem/service/RegisteredService.java
+++ src/main/java/com/bnuz/propertyManagementSystem/service/RegisteredService.java
@@ -5,7 +5,17 @@ import com.bnuz.propertyManagementSystem.model.User;
 
 public interface RegisteredService {
 
+    /**
+     * 检查是否有该用户名
+     * @param username
+     * @return
+     */
     public Result checkUserName(String username);
 
+    /**
+     * 注册该用户
+     * @param user
+     * @return
+     */
     public Result registeredUser(User user);
 }

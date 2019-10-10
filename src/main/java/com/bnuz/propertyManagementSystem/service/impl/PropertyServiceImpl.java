2cc33265af1b020218b872798e2fcc6732dad6e9
Hovees He <hovees.he@cargosmart.com>
2019-10-10 17:14:49 +0800

    楼盘service所有get方法加上了@Transactional(readOnly = true)

--- src/main/java/com/bnuz/propertyManagementSystem/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/bnuz/propertyManagementSystem/service/impl/PropertyServiceImpl.java
@@ -42,6 +42,7 @@ public class PropertyServiceImpl implements PropertyService {
   }
 
   @Override
+  @Transactional(readOnly = true)
   public Result findAll(Integer pageNum, Integer pageSize) {
     Pageable pageable = PageRequest.of(pageNum,pageSize);
     Page page = propertyDao.findAll(pageable);
@@ -49,18 +50,21 @@ public class PropertyServiceImpl implements PropertyService {
   }
 
   @Override
+  @Transactional(readOnly = true)
   public Result getById(Integer id) {
     Property property = propertyDao.getById(id);
     return new Result(true, ResultStatusCode.OK, "查询成功", property);
   }
 
   @Override
+  @Transactional(readOnly = true)
   public Result getByUid(Integer uid) {
     Property property = propertyDao.getByUid(uid);
     return new Result(true, ResultStatusCode.OK, "查询成功", property);
   }
 
   @Override
+  @Transactional(readOnly = true)
   public Result getByName(String propertyName) {
     Property property = propertyDao.getByName(propertyName);
     if(property != null) {
